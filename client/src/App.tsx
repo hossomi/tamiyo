a9a1e7c43edf21a83f867de129c5a9903f1d2e13
Marcelo Hossomi <marcelo.hossomi@gmail.com>
2022-11-03 16:58:40 -0300

    Make client fetch greeting from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("Loading...")
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.text())
+      .then(setText)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {text}
         </p>
         <a
           className="App-link"
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello!')
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(8000, () => console.log("Listening"))
